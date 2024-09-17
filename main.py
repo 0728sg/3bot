d74409487d7afd5c9c9e17e7f37696a679efb044
Cheremuxa <gaukhar.2006@bk.ru>
2024-09-17 19:18:11 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import logging
+from aiogram.utils import executor
+from buttons import start_test
+from config import bot, dp, admin
+from handlers import commands, echo, quiz, FSM_reg, FSM_store
+from db import db_main
+
+async def on_startup(_):
+    for i in admin:
+        await bot.send_message(chat_id=i, text="Бот включен!",
+                               reply_markup=start_test)
+        await db_main.sql_create()
+
+
+commands.register_commands(dp)
+quiz.register_quiz(dp)
+FSM_reg.register_fsm_reg(dp)
+FSM_store.register_store(dp)
+
+
+# echo.register_echo(dp)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
